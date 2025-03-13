4fe19390022387e2a6c75a7748a458796383d9c3
rrainerss <rainersstrads8@gmail.com>
2025-03-13 21:35:51 +0200

    Finished layout of ContactUs page. Needs color and spacing adjustment

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+    //For submitting with svelte
+    let firstName = '';
+    let lastName = '';
+    let email = '';
+    let phone = '';
+    let message = '';
+</script>
+
 <style>
     /*Contact information circles*/
     .CircleLight {
@@ -9,21 +18,54 @@
         right: -5em;
     }
     /*Input field color changes*/
-    input[type="text"] {
-        color: gray;
-        outline: none;
-        transition: all 0.2s ease-in-out;
-        color: black;
+    input {
+    outline: none;
+    transition: all 0.2s ease-in-out;
     }
-    input[type="text"]:focus {
+    input:focus {
+        color: black;
         border-color: black;
     }
-    .InputDiv:focus-within label {
+    .Label {
+        color: gray;
+        transition: color 0.2s ease-in-out;
+    }
+    .FocusedLabel {
         color: black;
+    }
+    /*Checkbox*/
+    input[type="checkbox"] {
+        appearance: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        width: 1.35em;
+        height: 1.35em;
+        border-radius: 50%;
+        border: .175em solid lightgray;
+        display: inline-block;
+        position: relative;
+        background-color: white;
+        transition: all 0.3s ease;
+    }
+    input[type="checkbox"]:checked {
+        background-color: black;
+        border-color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    input[type="checkbox"]:checked::before {
+        content: '';
+        margin-top: -0.075em;
+        width: .5em;
+        height: .75em;
+        border: solid white;
+        border-width: 0 .15em .15em 0;
+        transform: rotate(45deg);
     }
 </style>
 
-<div class="align-center flex justify-center flex-wrap pt-16 pb-16 bg-contact-bg">
+<div class="align-center flex justify-center flex-wrap pt-16 pb-16 px-4 bg-contact-bg">
     <!--tailwind notes
         content/h1 padding from top -> mt-16
         h1 size -> 5xl
@@ -39,35 +81,93 @@
             <h2 class="text-3xl w-full font-bold text-white">Contact information</h2>
             <p class="w-full mt-4 mb-4 text-gray-200">Say something to start a live chat!</p>
             <div class="z-30 relative md:mb-48">
-                <p class="text-white mt-24"><a href="tel:+371 12345678">+371 12345678</a></p>
-                <p class="text-white mt-12"><a href="mailto:epasts@inbox.lv">epasts@inbox.lv</a></p>
-                <p class="text-white mt-12">ALUS STREET 5, VALMIERA, VIDZEME, ESTONIA</p>
+                <p class="text-white mt-8 sm:mt-24">
+                    <img src="/icons/phone.svg" alt="Icon" class="inline-block align-middle w-6 h-6 me-4" />
+                    <a href="tel:+371 12345678">+371 12345678</a>
+                </p>
+                <p class="text-white mt-12">
+                    <img src="/icons/mail.svg" alt="Icon" class="inline-block align-middle w-6 h-6 me-4" />
+                    <a href="mailto:epasts@inbox.lv">epasts@inbox.lv</a>
+                </p>
+                <p class="text-white mt-12">
+                    <img src="/icons/location.svg" alt="Icon" class="inline-block align-middle w-6 h-6 me-4" />
+                    ALUS STREET 5, VALMIERA, VIDZEME, ESTONIA
+                </p>
             </div>
             <div class="CircleLight absolute bg-contact-green-light rounded-full w-35 h-35 z-20"></div>
             <div class="CircleDark absolute bg-contact-green-dark rounded-full w-70 h-70 z-10"></div>
         </div>
 
-        <form action="" class="w-full md:w-3/5 p-4">
-            <!--Forms-->
-            <div class="grid grid-flow-row grid-rows-2 grid-cols-2">
-                <div class="InputDiv px-8 py-6 w-full">
-                    <label class="text-sm text-gray-400" for="firstname">First Name:</label><br>
-                    <input type="text" name="firstname" placeholder="John" id="firstname" class="border-b border-gray-300 w-full py-2">
+        <form action="" class="w-full md:w-3/5 p-4 flex flex-col items-start">
+        <!-- <form on:submit={handleSubmit} class="w-full md:w-3/5 p-4"> -->
+            <div class="flex flex-wrap w-full">
+                <div class="InputDiv px-8 py-6 w-full md:w-1/2">
+                    <label class="text-sm Label {firstName ? 'FocusedLabel' : ''}" for="firstname">
+                        First Name:
+                    </label>
+                    <br>
+                    <input type="text" bind:value={firstName} placeholder="John" id="firstname" class="border-b border-gray-300 w-full py-2">
                 </div>
-                <div class="InputDiv px-8 py-6">
-                    <label class="text-sm text-gray-400" for="lastname">Last Name:</label><br>
-                    <input type="text" name="lastname" placeholder="Garlic" id="lastname" class="border-b border-gray-300 w-full py-2">
+                <div class="InputDiv px-8 py-6 w-full md:w-1/2">
+                    <label class="text-sm Label {lastName ? 'FocusedLabel' : ''}" for="lastname">
+                        Last Name:
+                    </label>
+                    <br>
+                    <input type="text" bind:value={lastName} placeholder="Garlic" id="lastname" class="border-b border-gray-300 w-full py-2">
                 </div>
-                <div class="InputDiv px-8 py-6">
-                    <label class="text-sm text-gray-400" for="email">E-mail:</label><br>
-                    <input type="text" name="email" placeholder="john@inbox.lv" id="email" class="border-b border-gray-300 w-full py-2">
+                <div class="InputDiv px-8 py-6 w-full md:w-1/2">
+                    <label class="text-sm Label {email ? 'FocusedLabel' : ''}" for="email">
+                        E-mail:
+                    </label>
+                    <br>
+                    <input type="text" bind:value={email} placeholder="john@inbox.lv" id="email" class="border-b border-gray-300 w-full py-2">
                 </div>
-                <div class="InputDiv px-8 py-6">
-                    <label class="text-sm text-gray-400" for="phone">Phone number:</label><br>
-                    <input type="text" name="phone" placeholder="+371 12345678" id="phone" class="border-b border-gray-300 w-full py-2">
+                <div class="InputDiv px-8 py-6 w-full md:w-1/2">
+                    <label class="text-sm Label {phone ? 'FocusedLabel' : ''}" for="phone">
+                        Phone number:
+                    </label>
+                    <br>
+                    <input type="text" bind:value={phone} placeholder="+371 12345678" id="phone" class="border-b border-gray-300 w-full py-2">
                 </div>
             </div>
-        </form>
 
+            <div class="CheckboxDiv px-8 py-6 flex flex-row flex-wrap w-auto">
+                <p class="w-full">Select subject:</p>
+                <label class="flex items-center space-x-2 cursor-pointer pe-4 mt-4 w-full sm:w-auto">
+                  <input type="checkbox">
+                  <p class="text-gray-700 text-sm m-0">General inquiry</p>
+                </label>
+                <label class="flex items-center space-x-2 cursor-pointer pe-4 mt-4 w-full sm:w-auto">
+                    <input type="checkbox">
+                    <p class="text-gray-700 text-sm m-0">Got robbed</p>
+                </label>
+                <label class="flex items-center space-x-2 cursor-pointer pe-4 mt-4 w-full sm:w-auto">
+                    <input type="checkbox">
+                    <p class="text-gray-700 text-sm m-0">Lonely</p>
+                </label>
+                <label class="flex items-center space-x-2 cursor-pointer pe-4 mt-4 w-full sm:w-auto">
+                    <input type="checkbox">
+                    <p class="text-gray-700 text-sm m-0">Need to talk</p>
+                </label>
+                <label class="flex items-center space-x-2 cursor-pointer pe-4 mt-4 w-full sm:w-auto">
+                    <input type="checkbox">
+                    <p class="text-gray-700 text-sm m-0">Hotel? Trivago</p>
+                </label>
+            </div>
+
+            <div class="flex flex-wrap w-full">
+                <div class="InputDiv px-8 py-6 w-full">
+                    <label class="text-sm Label {message ? 'FocusedLabel' : ''}" for="message">
+                        Message:
+                    </label>
+                    <br>
+                    <input type="text" bind:value={message} placeholder="John" id="message" class="border-b border-gray-300 w-full py-2">
+                </div>
+            </div>
+
+            <div class="flex flex-wrap w-full px-8 py-6 justify-center md:justify-end">
+                <input type="submit" value="Send Message" class="bg-black cursor-pointer hover:bg-gray-800 text-white py-4 px-12 border border-black rounded-lg">
+            </div>
+        </form>
     </div>
 </div>
