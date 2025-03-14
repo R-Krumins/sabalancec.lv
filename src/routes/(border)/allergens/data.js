3174e812d9be821cc4ff9877c33680c5b12068a4
rrainerss <rainersstrads8@gmail.com>
2025-03-14 16:17:44 +0200

    Created allergens page, updated image source to SRC folder, updated some fonts in contacts

---
+++ src/routes/(border)/allergens/data.js
@@ -0,0 +1,99 @@
+import corn from '$lib/images/allergens/cards/corn.png';
+import eggs from '$lib/images/allergens/cards/eggs.png';
+import fish from '$lib/images/allergens/cards/fish.png';
+import fruits from '$lib/images/allergens/cards/fruits.png';
+import gelatin from '$lib/images/allergens/cards/gelatin.png';
+import meat from '$lib/images/allergens/cards/meat.png';
+import milk from '$lib/images/allergens/cards/milk.png';
+import peanut from '$lib/images/allergens/cards/peanut.png';
+import seed from '$lib/images/allergens/cards/seed.png';
+import sesame from '$lib/images/allergens/cards/sesame.png';
+import shell_fish from '$lib/images/allergens/cards/shell-fish.png';
+import soy from '$lib/images/allergens/cards/soy.png';
+import spice from '$lib/images/allergens/cards/spice.png';
+import tree_nut from '$lib/images/allergens/cards/tree-nut.png';
+import tree_pollen from '$lib/images/allergens/cards/tree-pollen.png';
+import wheat from '$lib/images/allergens/cards/wheat.png';
+
+export const cardData = [
+    {
+        title: "Corn",
+        image: corn,
+        url: "/allergens/corn"
+    },
+    {
+        title: "Fish",
+        image: fish,
+        url: "/allergens/fish"
+    },
+    {
+        title: "Milk",
+        image: milk,
+        url: "/allergens/milk"
+    },
+    {
+        title: "Eggs",
+        image: eggs,
+        url: "/allergens/eggs"
+    },
+    {
+        title: "Fruits",
+        image: fruits,
+        url: "/allergens/fruits"
+    },
+    {
+        title: "Gelatin",
+        image: gelatin,
+        url: "/allergens/gelatin"
+    },
+    {
+        title: "Meat",
+        image: meat,
+        url: "/allergens/meat"
+    },
+    {
+        title: "Peanut",
+        image: peanut,
+        url: "/allergens/peanut"
+    },
+    {
+        title: "Seed",
+        image: seed,
+        url: "/allergens/seed"
+    },
+    {
+        title: "Sesame",
+        image: sesame,
+        url: "/allergens/sesame"
+    },
+    {
+        title: "Shell Fish",
+        image: shell_fish,
+        url: "/allergens/shell-fish"
+    },
+    {
+        title: "Soy",
+        image: soy,
+        url: "/allergens/soy"
+    },
+    {
+        title: "Spice",
+        image: spice,
+        url: "/allergens/spice"
+    },
+    {
+        title: "Tree Nut",
+        image: tree_nut,
+        url: "/allergens/tree-nut"
+    },
+    {
+        title: "Tree Pollen",
+        image: tree_pollen,
+        url: "/allergens/tree-pollen"
+    },
+    {
+        title: "Wheat",
+        image: wheat,
+        url: "/allergens/wheat"
+    }
+];
