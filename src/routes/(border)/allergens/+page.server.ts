f17fd363158f06a6a5140a8fd6ef559bea7a2855
rrainerss <rainersstrads8@gmail.com>
2025-03-24 13:49:23 +0200

    Individual allergen page loads data from API

--- src/routes/(border)/allergens/+page.server.ts
+++ src/routes/(border)/allergens/+page.server.ts
@@ -1,9 +1,10 @@
 export const load = async () => {
     const fetchAllergens = async () => {
         try {
+            //API call on server only
             const response = await fetch('https://sabalancec-warehouse-sanv8.ondigitalocean.app/api/allergen')
             const result = await response.json()
-            console.log("Data fetched!")
+            // console.log("All allergen data fetched!")
             return {
                 allergens: result.data
             }
@@ -12,6 +13,6 @@ export const load = async () => {
             console.log(error)
         }
     }
-    const data = await fetchAllergens(); //Await necessary to get data out of a "promise"
+    const data = await fetchAllergens(); //Await is necessary to get data out of a "promise"
     return data;
 }
