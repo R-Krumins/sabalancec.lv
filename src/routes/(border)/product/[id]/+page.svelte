52351cc4396f262d38b0b127271f63fa13c71a7d
Rainers K. <rainerkrum@gmail.com>
2025-03-27 00:44:42 +0200

    banner and info sections

--- src/routes/(border)/product/[id]/+page.svelte
+++ src/routes/(border)/product/[id]/+page.svelte
@@ -1,14 +1,20 @@
 <script>
+	import Banner from './Banner.svelte';
+	import Info from './Info.svelte';
+	import Hdecor from '$lib/components/Hdecor.svelte';
+
+	import BackArrow from '$lib/images/common/arrow-back.svg';
+
 	export let data;
 	console.log(data);
 </script>
 
 <div class="mx-10">
-	<div
-		class="h-[350px] flex-col rounded-4xl bg-[url(/frontpage/hero-bg.png)] bg-cover bg-center pt-16 pl-[10%]"
-	>
-		<div class="inline-block -rotate-2 bg-[#EBD96B] px-4 py-2">
-			<h1 class="poppins-extrabold inline-block rotate-2 text-5xl">{data.name}</h1>
-		</div>
-	</div>
+	<a href="/catalogue" class="flex gap-3">
+		<img src={BackArrow} alt="back arrow" />
+		<p>Catalogue</p>
+	</a>
+	<Banner name={data.name} image="/frontpage/hero-bg.png" />
+	<Hdecor text={data.name} />
+	<Info price={data.price} reviews={null} sales={data.saleCount} />
 </div>
---
+++ src/routes/(border)/product/[id]/Banner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	type Props = {
+		name: string;
+		image: string;
+	};
+	const { name, image }: Props = $props();
+</script>
+
+<div class="h-[350px] flex-col rounded-4xl bg-[url({image})] bg-cover bg-center pt-16 pl-[10%]">
+	<div class="inline-block -rotate-2 bg-[#EBD96B] px-4 py-2">
+		<h1 class="poppins-extrabold inline-block rotate-2 text-5xl">{name}</h1>
+	</div>
+</div>
