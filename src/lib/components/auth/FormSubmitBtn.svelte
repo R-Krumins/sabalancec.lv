d62402ef028f4edca82a134f885c4225fc13be68
Rainers K. <rainerkrum@gmail.com>
2025-03-27 23:13:42 +0200

    auth button animations

--- src/lib/components/auth/FormSubmitBtn.svelte
+++ src/lib/components/auth/FormSubmitBtn.svelte
@@ -1,3 +1,6 @@
-<button type="submit" class="poppins-bold mt-8 w-full rounded-xl bg-[#42531C] p-2 text-white">
+<button
+	type="submit"
+	class="poppins-bold mt-8 w-full rounded-xl bg-[#42531C] p-2 text-white hover:cursor-pointer hover:bg-[#5a6e2a] active:bg-[#2e3b14]"
+>
 	<slot />
 </button>
